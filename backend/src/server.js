35af407a1c04e564e6bffeba01c234ba55c63835
Vinicius Okamoto <leo_kamoto@hotmail.com>
2019-08-07 10:35:08 -0300

    Mongoose e inclusão de primeiro dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const routes = require('./routes');
 const server = express();
 
 //GET, POST criar tipo de registro, PUT editar algum registro, DELETE
 
+mongoose.connect('mongodb+srv://User:user@cluster0-gyipz.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+    useNewUrlParser: true
+});
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
