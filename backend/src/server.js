b6bffd0a0163298e7904a831b3a255258b474a9c
Vinicius Okamoto <leo_kamoto@hotmail.com>
2019-08-07 21:23:39 -0300

    Finalização BackEnd, Inicialização Front End

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cors = require('cors');
 const routes = require('./routes');
 const server = express();
 
@@ -8,6 +9,8 @@ const server = express();
 mongoose.connect('mongodb+srv://User:user@cluster0-gyipz.mongodb.net/omnistack8?retryWrites=true&w=majority',{
     useNewUrlParser: true
 });
+
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
