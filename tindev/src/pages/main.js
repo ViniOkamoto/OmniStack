7af421c49a9797e839a9a82f4e954403d8fc0f4d
Vinicius Okamoto <leo_kamoto@hotmail.com>
2019-08-16 19:56:44 -0300

    commit main terminada

--- tindev/src/pages/main.js
+++ tindev/src/pages/main.js
@@ -1,17 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
 import logo from '../assets/logo.png';
 import api from '../services/api';
 
-export default function Main({navigation}) {
+export default function Main({ navigation }) {
     const id = navigation.getParam('user')
     const [users, setUsers] = useState([]);
 
     console.log(id)
     console.log(users.length)
-    
+
     useEffect(() => {
         async function loadUsers() {
             const response = await api.get('/devs', {
@@ -24,54 +25,80 @@ export default function Main({navigation}) {
         loadUsers();
 
     }, [id]);
+/* handleLike e handleDislike são funções onde estão sendo chamadas no botão 
+    like e dislike, onde capturarão os usurários que estão disponíveis após o 
+    clique
+    */
+    async function handleLike() {
+        const [user, ...rest] = users;
+        /* aqui é onde há a captura dos users e o restos dos outros usuários,
+        essa é uma sintaxe que basicamente é equivalente a esta 
+        const user = users[0], porém esta consegue capturar o resto da array.  */
+        
+        await api.post(`devs/${user._id}/likes`, null, {
+            headers: { user: id },
+        })
+        /* aqui é onde denominamos qual usuário esta sofrend com o evento user.id
+            indicando para api que ocorreu algo */
+        setUsers(rest);
+        /* set users será a atualização da página após o evento anterior. */
+    };
 
-    async function handleLike(id) {
-        await api.post(`devs/${id}/likes`, null, {
+
+    async function handleDislike() {
+        const [user, ...rest] = users;
+
+        
+        await api.post(`devs/${user._id}/likes`, null, {
             headers: { user: id },
         })
 
-        setUsers(users.filter(users => users._id !== id));
+        setUsers(rest);
     };
 
-
-    async function handleDislike(id) {
-        await api.post(`devs/${id}/dislikes`, null, {
-            headers: { user: id },
-        });
-
-        setUsers(users.filter(users => users._id !== id));
-    };
+    async function handleLogout() {
+        await AsyncStorage.clear();
+        navigation.navigate('Login');
+    }
 
 
     return (
         <SafeAreaView style={styles.container}>
-            <Image style={styles.logo}source={logo} />
+            <TouchableOpacity onPress={handleLogout}>
+                <Image style={styles.logo} source={logo} />
+            </TouchableOpacity>
             <View style={styles.cardContainer}>
                 {users.length === 0
-                ? <Text style={styles.empty}>Acabou :(</Text> : (users.map((user, index) =>{
-                    <View key={user._id} style={[styles.card, {zIndex: users.length - index }]}>
-                    <Image style={styles.avatar} source={{uri: user.avatar}}/>
-                    <View style={styles.footer}>
-                        <Text style={styles.name}>{user.name}</Text>
-                        <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
-                    </View>
-                </View>        
-                })) }
+                    ? <Text style={styles.empty}>Acabou :(</Text> :
+                    (
+                        users.map((user, index) => (
+                            <View key={user._id} style={[styles.card, { zIndex: users.length - index }]}>
+                                <Image style={styles.avatar} source={{ uri: user.avatar }} />
+                                <View style={styles.footer}>
+                                    <Text style={styles.name}>{user.name}</Text>
+                                    <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
+                                </View>
+                            </View>
+                        ))
+                    )}
             </View>
-        <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-            <Image source={like}/>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-            <Image source={dislike}/>
-        </TouchableOpacity>
-        </View>
+           
+           {users.length > 0 && (
+                <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={handleLike}>
+                    <Image source={like} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleDislike}>
+                    <Image source={dislike} />
+                </TouchableOpacity>
+            </View>
+           )}
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: '#f5f5f5',
         alignItems: 'center',
@@ -81,7 +108,7 @@ const styles = StyleSheet.create({
     logo: {
         marginTop: 30,
     },
-    
+
     empty: {
         alignSelf: 'center',
         color: '#999',
@@ -129,16 +156,16 @@ const styles = StyleSheet.create({
     bio: {
         fontSize: 14,
         color: '#999',
-        marginTop:5,
+        marginTop: 5,
         lineHeight: 18,
     },
-    buttonContainer:{
-        flexDirection:'row',
+    buttonContainer: {
+        flexDirection: 'row',
         marginBottom: 30,
     },
-    button:{
-        width:50,
-        height:50,
+    button: {
+        width: 50,
+        height: 50,
         borderRadius: 25,
         backgroundColor: '#FFF',
         justifyContent: 'center',
