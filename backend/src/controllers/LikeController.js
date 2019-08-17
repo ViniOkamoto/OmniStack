4bf32e3d8d9da291aabe18e7886ec8fa56c5b5f3
Vinicius Okamoto <leo_kamoto@hotmail.com>
2019-08-17 00:21:34 -0300

    itsamacth sincronia ao vivo

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,25 +1,41 @@
 const Dev = require("../models/Dev");
+/* aqui é onde há o controle do like */
 
 module.exports = {
   async store(req,res){
+      console.log(req.io, req.connectedUsers);
       const {user} = req.headers;
       const {devId} = req.params;
-
+/* primeiro o servidor irá buscar o usuário e o id, que é o meio de identificarmos
+o usuário.*/
     const loggedDev = await Dev.findById(user);
     const targetDev = await Dev.findById(devId);
-
+/*pós isso atribuímos aos consts o usuário logado, e o alvo que estara 
+recebendo like, pois é a forma de identificarmos o evento */
     if(!targetDev){
         return res.status(400).json({error:'Dev not exists'});
+    /*este é um retorno caso o servidor não ache o usuário */
     }
 
     if(targetDev.likes.includes(loggedDev._id)){
-        console.log("match");
+        const loggedSocket = req.connectedUsers[user];
+        const targetSocket = req.connectedUsers[devId];
+
+        if(loggedSocket){
+            req.io.to(loggedSocket).emit('match', targetDev);
+        }
+
+        if(targetSocket) {
+            req.io.to(targetSocket).emit('match', loggedDev);
+        }
+    /*aqui verificamos se o dev que está recebendo like, foi recíproco
+    ele identificaram os likes e mandara no console a mensagem match */
     }
     loggedDev.likes.push(targetDev._id);
-
+    /*aqui empurramos para lista de likes o dev que está sendo curtido */
     await loggedDev.save();
-
+    /*aqui é onde salvamos o processo */
     return res.json(loggedDev);
-        
+    /*aqui é onde comunicamos ao servidor sobre o processo */    
     }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose')
+
 const cors = require('cors');
 const routes = require('./routes');
-const server = express();
-
+const app = express();
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+/* socket.io é uma forma de criarmos conexão entre o servidor e o externo,
+aqui basicamente estamos fazendo conexão com o http */
+const connectedUsers = {
+}
+io.on('connection', socket=>{
+    const { user } = socket.handshake.query;
+    console.log(user, socket.id);
+      connectedUsers[user] = socket.id
+});
 //GET, POST criar tipo de registro, PUT editar algum registro, DELETE
 
 mongoose.connect('mongodb+srv://User:user@cluster0-gyipz.mongodb.net/omnistack8?retryWrites=true&w=majority',{
     useNewUrlParser: true
 });
+// next é basicamente para dar continuidade ao fluxo da aplicação
+app.use((req,res,next) =>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
